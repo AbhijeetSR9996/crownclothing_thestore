91864868214b128d4bd11bfe4d44670e781cf07b
Abhijeet S.R <47415323+AbhijeetSR9996@users.noreply.github.com>
2025-02-22 23:55:32 +0530

    Update authentication.component.js

--- src/routes/authentication/authentication.component.js
+++ src/routes/authentication/authentication.component.js
@@ -1,12 +1,38 @@
 import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
 import SignInForm from "../../components/sign-in-form/sign-in-form.component";
-
+import Button from "../../components/button/button.component";
 import "./authentication.styles.scss";
 
 const Authentication = () => {
   return (
     <div className="authentication-container">
       <SignInForm />
+      {/* for LOGIN/SIGNUP Button */}
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          backgroundColor: "transparent",
+          marginLeft: "-400px",
+          marginRight: "110px",
+          rotate: "90deg",
+          //top: "220px",
+          //marginTop: "0px",
+        }}
+      >
+        <Button
+          style={{
+            fontSize: "95px",
+            cursor: "default",
+            pointerEvents: "none",
+          }}
+          buttonType={""}
+          disabled
+        >
+          LOGIN/SIGNUP
+        </Button>
+      </div>
+      {/* ---------X-----------*/}
       <SignUpForm />
     </div>
   );
